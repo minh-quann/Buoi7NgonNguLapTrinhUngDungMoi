bd63272673d3173c0f0f91697797cade122b98da
minh-quann <quannguyen19092003@gmail.com>
2025-03-20 21:46:27 +0700

    complete role

--- routes/roles.js
+++ routes/roles.js
@@ -1,29 +1,40 @@
 var express = require('express');
 var router = express.Router();
-let roleController = require('../controllers/roles')
-var {CreateSuccessRes,CreateErrorRes} = require('../utils/ResHandler')
+let roleController = require('../controllers/roles');
+var { CreateSuccessRes, CreateErrorRes } = require('../utils/ResHandler');
+const { verifyToken, checkRole } = require('../middlewares/auth');
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
-    let users = await roleController.GetAllRole();
-    CreateSuccessRes(res,200,users);
+router.get('/', async function (req, res, next) {
+  let roles = await roleController.GetAllRole();
+  CreateSuccessRes(res, 200, roles);
 });
-router.get('/:id', async function(req, res, next) {
-  try {
-    let user = await roleController.GetRoleById(req.params.id)
-    CreateSuccessRes(res,200,user);
-  } catch (error) {
-    next(error);
-  }
-});
-router.post('/', async function(req, res, next) {
+
+
+router.post('/', verifyToken, checkRole(['admin']), async function (req, res, next) {
   try {
     let newRole = await roleController.CreateRole(req.body.name);
-    CreateSuccessRes(res,200,newRole);
+    CreateSuccessRes(res, 200, newRole);
   } catch (error) {
     next(error);
   }
-})
+});
 
+router.put('/:id', verifyToken, checkRole(['admin']), async function (req, res, next) {
+  try {
+    let updatedRole = await roleController.UpdateRole(req.params.id, req.body);
+    CreateSuccessRes(res, 200, updatedRole);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', verifyToken, checkRole(['admin']), async function (req, res, next) {
+  try {
+    let deleteRole = await roleController.DeleteRole(req.params.id);
+    CreateSuccessRes(res, 200, deleteRole);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
